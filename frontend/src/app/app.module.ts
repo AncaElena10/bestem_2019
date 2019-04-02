3c6608f9f71dca5ac972f8220477ddf62b681093
Anca-Elena MOISĂ (67181) <anca.moisa>
2019-04-03 01:10:06 +0300

    added some components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
